598b3c19969e061f7e4c509269795248d7119bff
Snackm1x <jamesalexchilders@gmail.com>
2017-09-04 17:31:20 -0500

    Authentication Setup
    Set up routes and authentication using Google OAuth. Connected MongoDB
    with the use of Mongoose and verified a user account is created upon
    accessing the google authentication route.

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport'); //Doesn't export anything so we can just require it to make sure it's ran
+
+mongoose.connect(keys.mongoURI); //2nd part is to squish the warning in the terminal 
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        }, 
-        (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile', profile);
-        }
-    )
-); 
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in milliseconds
+        keys: [keys.cookieKey]
     })
 );
 
-app.get(
-    '/auth/google/callback',
-    passport.authenticate('google')
-);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+require('./routes/authRoutes')(app); //Calls the export from authRoutes function and passes the app as the parameter
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
