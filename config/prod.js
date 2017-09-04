e0998257f1dc88d1f79846756d7a2ffd44f7d2ad
Snackm1x <jamesalexchilders@gmail.com>
2017-09-04 17:46:50 -0500

    tweak google strategy settings

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    googleClientID: '676337157013-6si4qvnp3gpoveavglg9orn1s5k2v4og.apps.googleusercontent.com',
-    googleClientSecret: 'gQv630rh5rI8mC_ofjIQkYqR',
-    mongoURI: 'mongodb://admin:Theonly1!@ds121014.mlab.com:21014/snackemaily-prod',
-    cookieKey: 'poibuzxop8c9vu908234j51lk23nma0lkxcvp98asdf'
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
 };
